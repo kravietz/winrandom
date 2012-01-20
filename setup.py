96667f6a9e48ccdf915700e508ee73000594a590
Pawel Krawczyk <pawel.krawczyk@hush.com>
2012-01-20 20:55:52 +0100

    version 1.2

--- setup.py
+++ setup.py
@@ -1,37 +1,43 @@
 from distutils.core import setup, Extension
 
 winrandom1 = Extension('winrandom',
-	libraries = ['Crypt32'],
-	sources = ['src/winrandom.c'])
+	libraries=['Crypt32'],
+	sources=['src/winrandom.c'])
 
-setup (name = 'winrandom',
+setup (name='winrandom',
 	author='Pawel Krawczyk',
 	author_email='pawel.krawczyk@hush.com',
 	url='http://ipsec.pl/winrandom',
-	version = '1.1',
-	description = 'Access to Cryptographic API random generator',
-	long_description = """
-This very simple module gives direct access to Windows Cryptographic
-API CryptGetRandom() function.
+	version='1.2',
+	description='Access to Cryptographic API random generator',
+	long_description="""
+This module gives direct access to Windows Cryptographic
+API CryptGetRandom() function. The range() function
+uses B.5.1.1 Simple Discard Method from NIST SP800-90
+with FIPS 140-2 p. 44 Continuous random number generator test.
 
-Example:
+Examples:
 
 >>> import winrandom
 >>> print winrandom.long()
 2141228967
 >>> print repr(winrandom.bytes(10))
-<ASCII ENCODED JUNK>
+<10 random bytes>
+>> print winrandom.range(1000)
+123
 
 Changelog:
 1.0	added winrandom.long()
 1.1	added winrandom.bytes(num) returning num random bytes
+1.2 added winrandom.range(max) returning random index n where 0 <= n < max
 """,
-	license = "Public domain",
-	platforms = ["Win32"],
-	classifiers = ['Development Status :: 3 - Alpha',
+	license="Public domain",
+	platforms=["Win32"],
+	classifiers=['Classifier: Development Status :: 5 - Production/Stable',
           'License :: Public Domain',
 	  'Topic :: Security :: Cryptography',
 	  'Programming Language :: C',
+	  'Classifier: Programming Language :: Python',
           'Intended Audience :: Developers',
           'Operating System :: Microsoft :: Windows'],
-	ext_modules = [winrandom1])
+	ext_modules=[winrandom1])
